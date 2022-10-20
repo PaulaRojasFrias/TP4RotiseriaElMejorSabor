877e36bd55bd71f95bf4c1aa648e0d5cefa21f36
Paula Rojas Frias <rojasfriaspaula@gmail.com>
2022-10-20 15:08:55 -0300

    modelos en empleado

--- apps/empleado/models.py
+++ apps/empleado/models.py
@@ -1,4 +1,31 @@
 from django.db import models
 
 # Create your models here.
+#modelos empleado cadete
 
+class Empleado(models.Model):
+    cuit = models.CharField(max_length=8, unique=True)
+    nombre_completo = models.CharField(max_length=200)
+    fecha_nacimiento = models.DateField()
+    telefono_fijo = models.CharField(max_length=15)
+    telefono_celular = models.CharField(max_length=15)
+    calle= models.CharField(max_length=50)
+    numero= models.models.PositiveIntegerField
+    localidad= models.CharField(max_length=50)
+    departamento= models.CharField(max_length=50)
+    fecha_ingreso = models.DateField()
+
+class Cadete(models.Model):
+    cuit = models.CharField(max_length=8, unique=True)
+    nombre_completo = models.CharField(max_length=200)
+    fecha_nacimiento = models.DateField()
+    telefono_fijo = models.CharField(max_length=15)
+    telefono_celular = models.CharField(max_length=15)
+    calle = models.CharField(max_length=50)
+    numero = models.models.PositiveIntegerField
+    localidad = models.CharField(max_length=50)
+    departamento = models.CharField(max_length=50)
+    fecha_ingreso = models.DateField()
+    vigencia_carnet = models.DateField()
+    patente= models.CharField(max_length=10)
+    codigo_zona = models.CharField(max_length=10)
--- apps/estadistica/models.py
+++ apps/estadistica/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+# cada una de las estadisticas es una clase y sus atributos son la salida?
+
+# Listado de ventas diarias
+
+# Listado de cantidades entregadas por determinado cadete.
+
