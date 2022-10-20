dd76cedb30582814a2e3efeb48bb9682ced3176f
Lourdes Bazán Cisneros <lourdes_bazan2014@hotmail.com>
2022-10-20 16:05:54 -0300

    agregue atributos cliente

--- apps/cliente/models.py
+++ apps/cliente/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    cuil = models.CharField(max_length=11, unique=True)
+    nombre = models.CharField(max_length=200)
+    domCalle = models.CharField(max_length=100)
+    domNumero = models.CharField(max_length=4, unique=True)
+    domBarrio = models.CharField(max_length=100)
+    domLocalidad = models.CharField(max_length=100)
+    domObservacion = models.CharField(max_length=200)
+    domCodZona = models.CharField(max_length=5)
+    telefono = models.CharField(max_length=12, unique=True)
+    
+    creado = models.DateTimeField(auto_now_add=True)
+    modificado = models.DateTimeField(auto_now=True)
+
+
+    class Meta:
+        ordering = ('nombre',)
+
+    def __str__(self):
+        return '{}'.format(self.nombre)
